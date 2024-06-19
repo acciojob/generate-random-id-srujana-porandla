ea437a3d5f3447913bcf16fee5fd4fb0eca7f145
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-19 10:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let result = '';
+  
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+	
 }
 
 // Do not change the code below.
